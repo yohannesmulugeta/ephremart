33b34f4a6995507f5bcfb9c229e1544d70bb9454
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-09-14 21:53:35 +0300

    Load final UX polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './mobile.css'
+import './ux.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
